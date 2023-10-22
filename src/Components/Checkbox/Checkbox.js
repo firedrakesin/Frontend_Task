cc50fdc53c13ba1b5a441a740c7ddf754313980d
rakeshKdigital <jackdaws45@gmail.com>
2023-10-22 15:42:27 +0530

    readme

--- src/Components/Checkbox/Checkbox.js
+++ src/Components/Checkbox/Checkbox.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
-import './Checkbox.css'; 
+import './Checkbox.css';
 
 const Checkbox = () => {
+  // Defined a state variable 'highlightedCell' and a function 'setHighlightedCell' to manage the highlighted cell.
   const [highlightedCell, setHighlightedCell] = useState(null);
 
+  // Created a function 'createChessboard' to generate the chessboard grid.
   const createChessboard = () => {
     const board = [];
+
+    // Loop through rows and columns to create the chessboard cells.
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
+        // Determined if the cell should be black or white based on row and column indices.
         const isBlack = (row + col) % 2 === 0;
+
+        // Defined CSS classes for the cell, including 'black' or 'white' for coloring and 'red' for highlighting.
         const cellClasses = `cell ${isBlack ? 'black' : 'white'} ${
           highlightedCell === `${row}-${col}` ? 'red' : ''
         }`;
 
+        // Created a cell element with a unique key, CSS classes, and an onClick handler to toggle cell highlighting.
         board.push(
           <div
             key={`${row}-${col}`}
@@ -22,12 +30,15 @@ const Checkbox = () => {
         );
       }
     }
+
     return board;
   };
 
   return (
-    <div className='chess'>
-      {createChessboard()}
+    <div className='board'>
+      <div className='chess'>
+        {createChessboard()}
+      </div>
     </div>
   );
 };
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import Checkbox from '../Checkbox/Checkbox'
+import React from 'react';
+import Checkbox from '../Checkbox/Checkbox';
 
 const Home = () => {
     return (
-        < >
+        <>
+          {/* This is the main component for the frontend task. */}
+
           <h1 className='heading'>Frontend Task</h1>
-          <p className='para'>Created a grid of 8x8, it have alternate color of black and white, starting from black
-            when clicked on any grid, it highlights in red.
+          {/* Render a heading with the class 'heading'. */}
+
+          <p className='para'>
+            Created a grid of 8x8, it has alternate colors of black and white, starting from black when clicked on any grid, it highlights in red.
           </p>
-          <Checkbox /> 
+          {/* Render a paragraph with the class 'para', describing the task. */}
+
+          <Checkbox />
+          {/* Render the Checkbox component within the main component. */}
         </>
     );
 }
-export default Home
 
-
-
+export default Home;
